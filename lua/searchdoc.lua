3fe1b74d48e9a3ce2bef34fbdbe7253b5e41aa45
Shojin Masuda <shomasd@gmail.com>
2021-09-10 00:07:06 +0900

    remove obsolete code

--- lua/searchdoc.lua
+++ lua/searchdoc.lua
@@ -1,10 +1,6 @@
 local api = vim.api
 
 local function run(input)
-  -- local typeinfo = "tableinternal "
-  -- for key, value in pairs(input) do
-  --   typeinfo = typeinfo .. key .. " "
-  -- end
   api.nvim_echo({{input}}, true, {})
 end
 
