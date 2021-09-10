a42d49cbaec81358bdb36df339423d5b157d16dc
Shojin Masuda <shomasd@gmail.com>
2021-09-10 20:57:30 +0900

    use curl

--- lua/searchdoc.fnl
+++ lua/searchdoc.fnl
@@ -1,5 +1,5 @@
 
-(local curl (require :http))
+(local curl (require :curl))
 ; (local api vim.api)
 
 (local response (curl.easy {
@@ -10,6 +10,6 @@
     ]
     :writefunction  io.stderr
             }))
-;(fn run [input]
-;  (api.nvim_echo [[response]] true {}))
-;{:run run}
+(fn run [input]
+  (api.nvim_echo [[response]] true {}))
+{:run run}
--- lua/searchdoc.lua
+++ lua/searchdoc.lua
@@ -1,3 +1,6 @@
-local curl = require("http")
+local curl = require("curl")
 local response = curl.easy({httpheader = {"X-Test-Header1: Header-Data1", "X-Test-Header2: Header-Data2"}, url = "http://httpbin.org/get", writefunction = io.stderr})
-return nil
+local function run(input)
+  return api.nvim_echo({{response}}, true, {})
+end
+return {run = run}
